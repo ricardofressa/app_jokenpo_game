15286e0dd4882830ca389a8190d9ff2e7c914cd0
Ricardo Cardoso <ricardo.cardoso@certponto.com.br>
2017-06-20 22:36:33 -0300

    Implement component top

--- index.android.js
+++ index.android.js
@@ -10,7 +10,8 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  Image
 } from 'react-native';
 
 export default class app_jokenpo_game extends Component {
@@ -18,7 +19,9 @@ export default class app_jokenpo_game extends Component {
   constructor(props) {
     super(props);
     
-    this.state = { choiceUser : '', choiceComputer : '' };
+    this.state = {  choiceUser : '', 
+                    choiceComputer : '',
+                    result : '' };
   }
 
   jokenpo(choiceUser){
@@ -34,22 +37,82 @@ export default class app_jokenpo_game extends Component {
       case 2 : choiceComputer = 'Tesoura'; break;
     }
 
+    var result = ''
+
+    if(choiceComputer == 'Pedra'){
+      if(choiceUser == 'Pedra'){
+        result = 'Empate';
+      }
+
+      if(choiceUser == 'Papel'){
+        result = 'Usuário ganhou';
+      }
+
+      if(choiceUser == 'Tesoura'){
+        result = 'Computador ganhou';
+      }
+    }
+
+    if(choiceComputer == 'Papel'){
+      if(choiceUser == 'Papel'){
+        result = 'Empate';
+      }
+
+      if(choiceUser == 'Tesoura'){
+        result = 'Usuário ganhou';
+      }
+
+      if(choiceUser == 'Pedra'){
+        result = 'Computador ganhou';
+      }
+    }
+
+    if(choiceComputer == 'Tesoura'){
+      if(choiceUser == 'Tesoura'){
+        result = 'Empate';
+      }
+
+      if(choiceUser == 'Pedra'){
+        result = 'Usuário ganhou';
+      }
+
+      if(choiceUser == 'Papel'){
+        result = 'Computador ganhou';
+      }
+    }
+
     this.setState({ choiceUser : choiceUser,
-                    choiceComputer : choiceComputer
+                    choiceComputer : choiceComputer,
+                    result : result
                   });
   }
+
 
   render() {
     return (
       <View>
+        <View>
+          <Top />
+        </View>
+
         <Text> Escolha do usuário : { this.state.choiceUser } </Text>
         <Text> Escolha do computador : { this.state.choiceComputer } </Text>
-        <Text> Resultado </Text>
+        <Text> Resultado : { this.state.result} </Text>
         <Button title="Pedra" onPress={ () => {this.jokenpo('Pedra')} } />
         <Button title="Papel" onPress={ () => {this.jokenpo('Papel')} } />
         <Button title="Tesoura" onPress={ () => {this.jokenpo('Tesoura')} } />
       </View>
     );
+  }
+}
+
+class Top extends Component {
+  render() {
+    return (
+        <View>
+          <Image source={ require('./imgs/jokenpo.png') } />
+        </View>
+      );
   }
 }
 AppRegistry.registerComponent('app_jokenpo_game', () => app_jokenpo_game);
